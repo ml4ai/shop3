822fbec2fae62526ec75bb364322637ed3af08c2
Robert P. Goldman <rpgoldman@goldman-tribe.org>
2020-02-17 19:38:05 -0600

    Improve accessors for operators and actions.

--- shop3/pddl/pddl.lisp
+++ shop3/pddl/pddl.lisp
@@ -221,6 +221,9 @@ add code for processing the actions that is parallel to process-operator."))
   effect
   (cost-fun nil))
 
+(defun pddl-action-name (pddl-action)
+  (first (pddl-action-head pddl-action)))
+
 (defmethod parse-domain-items :around ((domain equality-mixin) items)
   "Add the axiom that treats equality as a built-in predicate.  This should 
 later be compiled into find-satisfiers or something."
